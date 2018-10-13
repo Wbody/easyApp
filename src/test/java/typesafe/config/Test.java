9f78a19fa615b3b561db21aaa8e7ebc13565dbf5
Wbody <2903056737@qq.com>
2018-10-14 05:36:03 +0800

    引入typesafe.config

--- src/test/java/typesafe/config/Test.java
+++ src/test/java/typesafe/config/Test.java
@@ -1,4 +1,18 @@
 package typesafe.config;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 public class Test {
+
+    //合并两个配置
+    public static void main(String[] args) {
+        Config config = ConfigFactory.load("produce.json");
+        config.withFallback(ConfigFactory.load("application.json"));
+        config.checkValid(ConfigFactory.defaultReference(), "easyapp");
+        Config mysql = config.getConfig("easyapp").getConfig("mysql");
+        System.out.println(mysql.getString("username"));
+
+    }
+
 }
