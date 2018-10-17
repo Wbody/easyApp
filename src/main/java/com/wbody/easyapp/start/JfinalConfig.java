e3ca1ea6f163b16a384539a3ed79180ad618dbb5
Wbody <2903056737@qq.com>
2018-10-17 14:27:55 +0800

    添加druid配置

--- src/main/java/com/wbody/easyapp/start/JfinalConfig.java
+++ src/main/java/com/wbody/easyapp/start/JfinalConfig.java
@@ -1,15 +1,25 @@
 package com.wbody.easyapp.start;
 
+import com.alibaba.druid.filter.logging.Slf4jLogFilter;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.wall.WallFilter;
 import com.jfinal.config.*;
+import com.jfinal.core.JFinal;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.druid.DruidPlugin;
 import com.jfinal.template.Engine;
-import com.wbody.jfinal.ext.LogBackFactory;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import com.wbody.jfinal.kit.ExplorerKit;
 
 public class JfinalConfig extends JFinalConfig {
     @Override
     public void configConstant(Constants constants) {
-        constants.setLogFactory(new LogBackFactory());
-
+        Config config = ConfigFactory.load("application.json");
+        Config produce=ConfigFactory.load("produce.json");
+        config.resolveWith(produce);
+        config.checkValid(ConfigFactory.defaultReference(), "easyapp");
+        JFinal.me().getServletContext().setAttribute("config",config.getConfig("easyapp"));
     }
 
     @Override
@@ -24,6 +34,25 @@ public class JfinalConfig extends JFinalConfig {
 
     @Override
     public void configPlugin(Plugins plugins) {
+        Config config = (Config) JFinal.me().getServletContext().getAttribute("config");
+        Config mysql=config.getConfig("mysql");
+        DruidPlugin dp = new DruidPlugin(mysql.getString("url"), mysql.getString("username"), mysql.getString("password"), mysql.getString("driverClassName"));
+        dp.set(mysql.getInt("initialSize"),mysql.getInt("minIdle"),mysql.getInt("maxActive"));
+        StatFilter statFilter = new StatFilter();
+        statFilter.setMergeSql(true);
+        statFilter.setLogSlowSql(true);
+        dp.addFilter(statFilter);
+
+        WallFilter wall = new WallFilter();
+        wall.setDbType("mysql");
+        dp.addFilter(wall);
+
+        plugins.add(dp);
+
+
+        ActiveRecordPlugin arp = new ActiveRecordPlugin(dp);
+        arp.setShowSql(true);
+        plugins.add(arp);
 
     }
 
@@ -37,9 +66,14 @@ public class JfinalConfig extends JFinalConfig {
 
     }
 
+
+
     @Override
     public void afterJFinalStart() {
         super.afterJFinalStart();
         ExplorerKit.openExplorer("http://127.0.0.1:8080");
+
+
+
     }
 }
---
+++ src/main/java/com/wbody/jfinal/ext/EasyModel.java
@@ -0,0 +1,61 @@
+package com.wbody.jfinal.ext;
+
+import java.util.Map;
+
+public class EasyModel {
+
+    private String id;
+    private String code;
+    private String name;
+    private boolean active;
+    private int status;
+    private Map<String,Object> detail;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Object> getDetail() {
+        return detail;
+    }
+
+    public void setDetail(Map<String, Object> detail) {
+        this.detail = detail;
+    }
+}
